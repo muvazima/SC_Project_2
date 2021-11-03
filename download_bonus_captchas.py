002553b8e9a6c067af0cc07729a89ab3d844708b
Muvazima Mansoor <muvazima@Muvazimas-MacBook-Air.local>
2021-11-03 09:55:19 +0000

    bonus

---
+++ download_bonus_captchas.py
@@ -0,0 +1,12 @@
+import requests
+#username='mansoorm'
+#username='makarana'
+username='bonus'
+url='http://cs7ns1.scss.tcd.ie/2122/'+username+'/pi-project2-bonus/'
+r=requests.get(url)
+for i in r.text.split(',\n'):
+   captcha=requests.get(url+i)
+   file_loc=username+'_captchas/'+i
+   file=open(file_loc, 'wb')
+   file.write(captcha.content)
+   file.close()
