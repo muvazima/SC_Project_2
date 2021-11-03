f610fce4b8ba33d1e8f184014a73bbca4e7b8a30
Muvazima Mansoor <mansoorm@local.scss.tcd.ie>
2021-11-03 12:38:03 +0000

    bonus_captchas

--- download_bonus_captchas.py
+++ download_bonus_captchas.py
@@ -3,7 +3,7 @@ import requests
 #username='makarana'
 username='bonus'
 url='http://cs7ns1.scss.tcd.ie/2122/'+username+'/pi-project2-bonus/'
-r=requests.get(url)
+r=requests.get(url+username+'-challenge-filenames.csv')
 for i in r.text.split(',\n'):
    captcha=requests.get(url+i)
    file_loc=username+'_captchas/'+i
